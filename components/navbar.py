669d76cfb6eabd4beb0e4585fc7f23a74f65b7ef
nadim7 <74548439+nasa1900@users.noreply.github.com>
2022-11-22 08:06:54 +0530

    Add files via upload

---
+++ components/navbar.py
@@ -0,0 +1,23 @@
+# Import necessary libraries
+from dash import html
+import dash_bootstrap_components as dbc
+
+
+# Define the navbar structure
+def Navbar():
+
+    layout = html.Div([
+        dbc.NavbarSimple(
+            children=[
+                dbc.NavItem(dbc.NavLink("Home", href="/home")),
+                dbc.NavItem(dbc.NavLink("About Us", href="/page1")),
+                dbc.NavItem(dbc.NavLink("Contact Us", href="/page2")),
+            ] ,
+            brand="Badshah Algo World",
+            brand_href="/home",
+            color="dark",
+            dark=True,
+        ), 
+    ])
+
+    return layout
